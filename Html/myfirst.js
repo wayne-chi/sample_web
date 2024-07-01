c96f13690e7fbba64df1c955f5edddc986315374
Godswill Otto <godswillotto2@gmail.com>
2024-07-01 20:45:04 +0000

    added more dependencies

--- Html/myfirst.js
+++ Html/myfirst.js
@@ -2,7 +2,7 @@ var http = require('http');
 var dt = require('./myfirstmodule');
 http.createServer(function (req, res){
    res.writeHead(200, {'Content-Type':'text/html'});
-    res.write('Hello world! the time is ' + dt.myDateTime()+"  "+ req.url);
+    res.write('Hello world! the time is ' + dt.myDateTime()+" \n\n "+ req.url);
     
     res.end();
 }).listen(8080);
