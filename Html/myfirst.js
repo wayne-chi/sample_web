b95362c5c6cadbbcc4c4673d0134cc7e36fead72
Godswill Otto <godswillotto2@gmail.com>
2024-07-01 19:48:54 +0000

    extra scrpts

---
+++ Html/myfirst.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var dt = require('./myfirstmodule');
+http.createServer(function (req, res){
+   res.writeHead(200, {'Content-Type':'text/html'});
+    res.write('Hello world! the time is ' + dt.myDateTime()+"  "+ req.url);
+    
+    res.end();
+}).listen(3000);
